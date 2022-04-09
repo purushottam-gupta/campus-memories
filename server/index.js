a3b92b1f2821efe47494b296853b288cc513acb3
Purushottam Gupta <guptapurushottam123@gmail.com>
2022-04-09 22:59:03 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,34 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
+
+const app = express();
+
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use("/user", userRouter);
+
+const CONNECTION_URL = 'mongodb://localhost:27017/post';
+const PORT = process.env.PORT|| 5000;
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static("client/build"));
+  // const path=require("path");
+  // app.get("*", (req,res) => {
+  //   res,sendFile(path.resolve(__dirname,'client','build','index.html'));
+  // })
+}
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
